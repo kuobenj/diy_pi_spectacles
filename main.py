66cea5c36df0ad22552347c3e2911e43b6cca5b4
Status Kuo <kuobenj@gmail.com>
2017-12-05 15:58:33 -0500

    modified code to not crop and also a light demo mode

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import RPi.GPIO
+import random
 import time
 import datetime
+import os
 from picamera import PiCamera
 
 # Pin Definitons:
@@ -21,43 +23,57 @@ LEDList = [flashSPin, recordSPin, modeSPin, statusSPin]
 button2name = {shutdownBPin :'shutdownBPin', functionBPin : 'functionBPin', modeBPin : 'modeBPin', flashBPin : 'flashBPin'}
 
 flash_mode = False
-video_mode = False
+system_mode = 'photo' # other modes to be supported will be demo and video
 
 #camera object
 camera = PiCamera()
 
 def button_callback(button):
     global flash_mode
-    global video_mode
+    global system_mode
     global button2name
+
     print 'button pressed: ' + repr(button) + ' ' + button2name[button]
-    # because for now there's 4 buttons I can cheat a little and map each button to an LED to toggle for a test
-    # for i in range(0, len(ButtonList)):
-        # if button == ButtonList[i]:
-        #     RPi.GPIO.output(LEDList[i], not(RPi.GPIO.input(LEDList[i])))
 
-    if button == flashBPin:
-        flash_mode = not(flash_mode)
-        RPi.GPIO.output(modeSPin, not(RPi.GPIO.input(modeSPin)))
+    if system_mode == 'button_demo':
+        if button == modeBPin:
+            system_mode = 'photo'
+            flash_mode = False
+            # Bring LEDs down
+            for led in LEDList:
+                if led != statusSPin:
+                    RPi.GPIO.setup(led, RPi.GPIO.OUT)
+                    RPi.GPIO.output(led, RPi.GPIO.LOW)
+        else:
+            random_pin = random.randint(0,3)
+            RPi.GPIO.output(LEDList[random_pin], not(RPi.GPIO.input(LEDList[random_pin])))
 
-    if button == modeBPin:
-        video_mode = not(video_mode) #this will not be implemented at the moment
+    else: #for now this is just photos
+        if button == flashBPin:
+            flash_mode = not(flash_mode)
+            RPi.GPIO.output(modeSPin, not(RPi.GPIO.input(modeSPin)))
 
-    if button == functionBPin:
-        print 'flash_mode ' + repr(flash_mode)
-        if flash_mode:
-            RPi.GPIO.output(flashSPin, RPi.GPIO.HIGH)
-        camera.capture(datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S') + '.jpg')
-        RPi.GPIO.output(flashSPin, RPi.GPIO.LOW)
+        if button == modeBPin:
+            # video mode not implemented yet
+            system_mode = 'button_demo'
 
-    if button == shutdownBPin:
-        time.sleep(10)
-        if RPi.GPIO.input(shutdownBPin) == RPi.GPIO.LOW:
-           print 'SAFE SHUTDOWN COMMENCING (JUST A PRINT AT THE MOMENT)'
-           # still need to execute shutdown
-           # raise KeyboardInterrupt
-           RPi.GPIO.cleanup()
-           quit()
+        if button == functionBPin:
+            print 'flash_mode ' + repr(flash_mode)
+            if flash_mode:
+                RPi.GPIO.output(flashSPin, RPi.GPIO.HIGH)
+            camera.capture(datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S') + '.jpg')
+            RPi.GPIO.output(flashSPin, RPi.GPIO.LOW)
+
+        if button == shutdownBPin:
+            time.sleep(10)
+            if RPi.GPIO.input(shutdownBPin) == RPi.GPIO.LOW:
+               print 'SAFE SHUTDOWN COMMENCING'
+               # still need to execute shutdown
+               # raise KeyboardInterrupt
+               RPi.GPIO.cleanup()
+               time.sleep(5)
+               os.system('sudo shutdown -h now')
+               quit()
 
 # Initial Pin Setup:
 RPi.GPIO.setmode(RPi.GPIO.BOARD) # Broadcom pin-numbering scheme
@@ -73,7 +89,8 @@ for led in LEDList:
     RPi.GPIO.output(led, RPi.GPIO.HIGH)
 
 # Camera Setup
-camera.resolution = (1920, 1080)
+# camera.resolution = (1920, 1080)
+camera.resolution = (2592, 1944)
 camera.rotation = 270
 # camera.start_preview()
 # Camera warm-up time
